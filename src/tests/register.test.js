55b677054a6963d274a1813c337b30ad2925e30c
Kenneth Edwards <kennethedwards@Kenneths-MacBook-Pro.local>
2018-07-18 22:07:11 -0400

    changed nav for addact page

---
+++ src/tests/register.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import Register from '../components/register';
+
+describe ('Register', () => {
+
+    let register = mount(<Register/> )
+});
+
