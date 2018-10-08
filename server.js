1c2eaaff95d5a847a44638445b53962bcf2aad0f
Kenneth Edwards <kennethedwards@Kenneths-MacBook-Pro.local>
2018-10-08 02:15:55 -0400

    took out morgan and common

--- server.js
+++ server.js
@@ -3,7 +3,6 @@
 const express = require('express')
 
 express()
-  .use(require('morgan')('common'))
   .use(express.static(`${__dirname}/build`))
   .get('*', (req, res) => res.sendFile(`${__dirname}/build/index.html`))
   .listen(process.env.PORT, () => {
